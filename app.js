6a5f3b1276b65d8dd6b4b30a0373817be8375d77
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-05-24 23:43:18 +0100

    initial section 6 setup

--- app.js
+++ app.js
@@ -1,79 +1,31 @@
-const fs = require("fs")
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
 
-const app = express()
-app.use(express.json())
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
-})
+const app = express();
 
-
-
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-)
-
-const getAllTours = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
-const getSingleTour = (req, res) => {
-    const id = req.params.id * 1
-    const tour = tours.find(el => el.id === id)
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
-    if (id > tours.length) {
-        return res.status(404).send({ "message": "An Error occured" })
-    }
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    })
-}
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-const postTour = (req, res) => {
-
-    const newId = tours[tours.length - 1].id + 1
-    const newTour = Object.assign({ id: newId }, req.body)
-    tours.push(newTour)
-
-
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: success,
-            data: {
-                tour: newTour
-            }
-        })
-    })
-
-}
-
-const updateTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: 'Updated'
-        }
-    })
-}
-
-app.get('/', (req, res) => {
-    res.status(200).send('Hello from the server sideeeee.')
-})
-
-app.get('/api/v1/tours', getAllTours)
-app.get('/api/v1/tours/:id', getSingleTour)
-app.post('/api/v1/tours', postTour)
-app.patch('/api/v1/tours', updateTour)
+module.exports = app;
